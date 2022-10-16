cb6cda4b0ecd9444e76080f05382fb1320b6f451
Justin Handville <nanolith@gmail.com>
2022-10-16 17:55:45 -0400

    decl of psock_get_string.

--- include/rcprhelper/psock.h
+++ include/rcprhelper/psock.h
@@ -160,6 +160,21 @@ status RCPR_SYM(psock_handle_release)(RCPR_SYM(psock_handle)* handle);
  */
 status RCPR_SYM(psock_br_handle_release)(RCPR_SYM(psock_br_handle)* handle);
 
+/**
+ * \brief Unwrap a void string into a real string.
+ *
+ * This method is used by Idris to handle results that could be NULL.
+ *
+ * \note This assumes that the string is valid. It is up to the caller to verify
+ * that the string is valid (not NULL) before calling this function. Here be
+ * dragons.
+ *
+ * \param opaque_string         The string to unwrap.
+ *
+ * \returns the \p opaque_string cast as a char*.
+ */
+char* RCPR_SYM(psock_get_string)(void* opaque_string);
+
 /* C++ compatibility. */
 # ifdef   __cplusplus
 }
